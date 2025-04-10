6ad000cc2414134032de583f0a5c3916d6ec8dff
Dheeren <ganeshgaud1111@gmail.com>
2025-04-10 14:03:05 +0530

    cloud db Defalt

--- db/index.js
+++ db/index.js
@@ -2,32 +2,34 @@ import mongoose from "mongoose";
 import  {DB_NAME}  from "../constants.js";
 
 
-// const connectDB = async () => {
-//     try {
-//         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
-//     } catch (error) {
-//         console.log("MONGODB connection FAILED ", error);
-//         process.exit(1)
-//     }
-// }
-
-
-
 const connectDB = async () => {
-    // console.log(DB_NAME);
+    console.log("Cloud Db Connection");
+    
     try {
-      await mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log("\n\ndatabase is connected...");
-      
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
-      console.log("some error in connecting database");
-      throw error; 
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
     }
-  };
+}
+
+
+
+// const connectDB = async () => {
+  // console.log("Local Db Connection");
+  //     try {
+//       await mongoose.connect(process.env.MONGODB_URI, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       });
+//       console.log("\n\ndatabase is connected...");
+      
+//     } catch (error) {
+//       console.log("some error in connecting database");
+//       throw error; 
+//     }
+//   };
   
 
 
